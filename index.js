e805f4fefb739145d8603d4ef4fc185541bf452f
Marquis Nobles <marquis@greatnessinabox.com>
2021-03-31 11:27:28 -0400

    Add video container id prop for tagging

--- index.js
+++ index.js
@@ -5,231 +5,259 @@ import BackgroundCover from 'background-cover';
 import playInlineVideo from 'iphone-inline-video';
 import insertRule from 'insert-rule';
 
-const iOSNavigator = typeof navigator !== 'undefined' && navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
+const iOSNavigator =
+	typeof navigator !== 'undefined' &&
+	navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
 const iOSVersion = iOSNavigator ? iOSNavigator[1] : null;
 
 const noop = () => {};
 export default class BackgroundVideo extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-    };
-    this.startTimeIsSet = false;
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			visible: false,
+		};
+		this.startTimeIsSet = false;
+	}
 
-  componentDidMount() {
-    if (this.props.playsInline && iOSVersion) {
-      const hasAudio = !(iOSVersion && iOSVersion < 10 && this.props.autoPlay && this.props.muted); // allow autoplay on iOS < 10 for silent videos
-      const requireInteractionOnTablet = false;
+	componentDidMount() {
+		if (this.props.playsInline && iOSVersion) {
+			const hasAudio = !(
+				iOSVersion &&
+				iOSVersion < 10 &&
+				this.props.autoPlay &&
+				this.props.muted
+			); // allow autoplay on iOS < 10 for silent videos
+			const requireInteractionOnTablet = false;
 
-      playInlineVideo(this.video, hasAudio, requireInteractionOnTablet);
-      insertRule(['video::-webkit-media-controls-start-playback-button', '.IIV::-webkit-media-controls-play-button'], {
-        display: 'none',
-      });
-    }
+			playInlineVideo(this.video, hasAudio, requireInteractionOnTablet);
+			insertRule(
+				[
+					'video::-webkit-media-controls-start-playback-button',
+					'.IIV::-webkit-media-controls-play-button',
+				],
+				{
+					display: 'none',
+				}
+			);
+		}
 
-    if (this.video) {
-      if (this.video.readyState !== 4) {
-        this.video.addEventListener('loadedmetadata', this._handleVideoReady);
-      } else {
-        this._handleVideoReady();
-      }
+		if (this.video) {
+			if (this.video.readyState !== 4) {
+				this.video.addEventListener('loadedmetadata', this._handleVideoReady);
+			} else {
+				this._handleVideoReady();
+			}
 
-      this.video.addEventListener('play', this._handleOnPlay);
-      this.video.addEventListener('pause', this._handleOnPause);
-      this.video.volume = this.props.volume;
-    }
-  }
+			this.video.addEventListener('play', this._handleOnPlay);
+			this.video.addEventListener('pause', this._handleOnPause);
+			this.video.volume = this.props.volume;
+		}
+	}
 
-  componentDidUpdate(prevProps) {
-    if (this.video && this.props.volume !== prevProps.volume) {
-      this.video.volume = this.props.volume;
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (this.video && this.props.volume !== prevProps.volume) {
+			this.video.volume = this.props.volume;
+		}
+	}
 
-  componentWillUnmount() {
-    if (!this.video) return;
-    this.video.removeEventListener('loadedmetadata', this._handleVideoReady);
-    this.video.removeEventListener('play', this._handleOnPlay);
-    this.video.removeEventListener('pause', this._handleOnPause);
-  }
+	componentWillUnmount() {
+		if (!this.video) return;
+		this.video.removeEventListener('loadedmetadata', this._handleVideoReady);
+		this.video.removeEventListener('play', this._handleOnPlay);
+		this.video.removeEventListener('pause', this._handleOnPause);
+	}
 
-  _handleVideoReady = () => {
-    this.setState({ visible: true });
-    this.props.startTime && this.setCurrentTime(this.props.startTime);
-    this.props.autoPlay && this.play();
-    this.video && this.props.onReady(this.video.duration);
-  };
+	_handleVideoReady = () => {
+		this.setState({ visible: true });
+		this.props.startTime && this.setCurrentTime(this.props.startTime);
+		this.props.autoPlay && this.play();
+		this.video && this.props.onReady(this.video.duration);
+	};
 
-  _resize() {
-    this.video && BackgroundCover(this.video, this.container, this.props.horizontalAlign, this.props.verticalAlign);
-  }
+	_resize() {
+		this.video &&
+			BackgroundCover(
+				this.video,
+				this.container,
+				this.props.horizontalAlign,
+				this.props.verticalAlign
+			);
+	}
 
-  _handleOnPlay = () => {
-    this.props.onPlay();
-  };
+	_handleOnPlay = () => {
+		this.props.onPlay();
+	};
 
-  _handleOnPause = () => {
-    this.props.onPause();
-  };
+	_handleOnPause = () => {
+		this.props.onPause();
+	};
 
-  _handleTimeUpdate = () => {
-    if (!this.video) return;
-    iOSVersion && this._handleIOSStartTime();
-    const currentTime = this.video.currentTime;
-    const duration = this.video.duration;
-    const progress = currentTime / duration;
-    this.props.onTimeUpdate(currentTime, progress, duration);
-  };
+	_handleTimeUpdate = () => {
+		if (!this.video) return;
+		iOSVersion && this._handleIOSStartTime();
+		const currentTime = this.video.currentTime;
+		const duration = this.video.duration;
+		const progress = currentTime / duration;
+		this.props.onTimeUpdate(currentTime, progress, duration);
+	};
 
-  _handleVideoEnd = () => {
-    this.props.onEnd();
-  };
+	_handleVideoEnd = () => {
+		this.props.onEnd();
+	};
 
-  _handleIOSStartTime() {
-    if (!this.video) return;
-    if (this.video.currentTime < this.props.startTime && !this.startTimeIsSet) {
-      this.setCurrentTime(this.props.startTime);
-      this.startTimeIsSet = true;
-    }
-  }
+	_handleIOSStartTime() {
+		if (!this.video) return;
+		if (this.video.currentTime < this.props.startTime && !this.startTimeIsSet) {
+			this.setCurrentTime(this.props.startTime);
+			this.startTimeIsSet = true;
+		}
+	}
 
-  play() {
-    this.video && this.video.play();
-  }
+	play() {
+		this.video && this.video.play();
+	}
 
-  pause() {
-    this.video && this.video.pause();
-  }
+	pause() {
+		this.video && this.video.pause();
+	}
 
-  togglePlay() {
-    if (!this.video) return;
-    this.video.paused ? this.play() : this.pause();
-  }
+	togglePlay() {
+		if (!this.video) return;
+		this.video.paused ? this.play() : this.pause();
+	}
 
-  isPaused() {
-    return this.video ? this.video.paused : false;
-  }
+	isPaused() {
+		return this.video ? this.video.paused : false;
+	}
 
-  mute() {
-    if (!this.video) return;
-    this.video.muted = true;
-    this.props.onMute();
-  }
+	mute() {
+		if (!this.video) return;
+		this.video.muted = true;
+		this.props.onMute();
+	}
 
-  unmute() {
-    if (!this.video) return;
-    this.video.muted = false;
-    this.props.onUnmute();
-  }
+	unmute() {
+		if (!this.video) return;
+		this.video.muted = false;
+		this.props.onUnmute();
+	}
 
-  toggleMute() {
-    if (!this.video) return;
-    this.video.muted ? this.unmute() : this.mute();
-  }
+	toggleMute() {
+		if (!this.video) return;
+		this.video.muted ? this.unmute() : this.mute();
+	}
 
-  isMuted() {
-    return this.video ? this.video.muted : false;
-  }
+	isMuted() {
+		return this.video ? this.video.muted : false;
+	}
 
-  setCurrentTime(val) {
-    if (!this.video) return;
-    this.video.currentTime = val;
-  }
+	setCurrentTime(val) {
+		if (!this.video) return;
+		this.video.currentTime = val;
+	}
 
-  render() {
-    const visibility = this.state.visible ? 'visible' : 'hidden';
+	render() {
+		const visibility = this.state.visible ? 'visible' : 'hidden';
 
-    const videoProps = {
-      ref: (v) => (this.video = v),
-      src: typeof this.props.src === 'string' ? this.props.src : null,
-      preload: this.props.preload,
-      poster: this.props.poster,
-      muted: this.props.muted,
-      loop: this.props.loop,
-      onTimeUpdate: this._handleTimeUpdate,
-      onEnded: this._handleVideoEnd,
-      ...Object.assign(this.props.extraVideoElementProps, {
-        playsInline: this.props.playsInline,
-      }),
-    };
+		const videoProps = {
+			ref: v => (this.video = v),
+			src: typeof this.props.src === 'string' ? this.props.src : null,
+			preload: this.props.preload,
+			poster: this.props.poster,
+			muted: this.props.muted,
+			loop: this.props.loop,
+			onTimeUpdate: this._handleTimeUpdate,
+			onEnded: this._handleVideoEnd,
+			...Object.assign(this.props.extraVideoElementProps, {
+				playsInline: this.props.playsInline,
+			}),
+		};
 
-    return (
-      <div
-        ref={r => (this.container = r)}
-        className={`BackgroundVideo ${this.props.className || ''}`}
-        style={Object.assign({ visibility }, this.props.style)}
-        onClick={this.props.onClick}
-        onKeyPress={this.props.onKeyPress}
-        tabIndex={this.props.tabIndex}>
-        {typeof this.props.src === 'object' ? (
-          <video {...videoProps} className={this.props.videoClasses} style={Object.assign({}, this.props.videoStyles)}>
-            {this.props.src.map((source, key) => (
-              <source key={key} {...source} />
-            ))}
-          </video>
-        ) : (
-          <video {...videoProps} className={this.props.videoClasses} style={Object.assign({}, this.props.videoStyles)} />
-        )}
-      </div>
-    );
-  }
+		return (
+			<div
+				id={this.props.videoContainerID}
+				ref={r => (this.container = r)}
+				className={`BackgroundVideo ${this.props.className || ''}`}
+				style={Object.assign({ visibility }, this.props.style)}
+				onClick={this.props.onClick}
+				onKeyPress={this.props.onKeyPress}
+				tabIndex={this.props.tabIndex}>
+				{typeof this.props.src === 'object' ? (
+					<video
+						{...videoProps}
+						className={this.props.videoClasses}
+						style={Object.assign({}, this.props.videoStyles)}>
+						{this.props.src.map((source, key) => (
+							<source key={key} {...source} />
+						))}
+					</video>
+				) : (
+					<video
+						{...videoProps}
+						className={this.props.videoClasses}
+						style={Object.assign({}, this.props.videoStyles)}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 BackgroundVideo.propTypes = {
-  playsInline: PropTypes.bool, // play inline on iPhone. avoid triggering native video player
-  disableBackgroundCover: PropTypes.bool, // do not apply cover effect (e.g. disable it for specific screen resolution or aspect ratio)
-  style: PropTypes.object,
-  videoStyles: PropTypes.object,
-  className: PropTypes.string,
-  videoClasses: PropTypes.string,
-  containerWidth: PropTypes.number,
-  containerHeight: PropTypes.number,
-  src: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
-  poster: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  horizontalAlign: PropTypes.number,
-  verticalAlign: PropTypes.number,
-  preload: PropTypes.string,
-  muted: PropTypes.bool, // required to be set to true for auto play on mobile in combination with 'autoPlay' option
-  volume: PropTypes.number,
-  loop: PropTypes.bool,
-  autoPlay: PropTypes.bool,
-  extraVideoElementProps: PropTypes.object,
-  startTime: PropTypes.number,
-  tabIndex: PropTypes.number,
-  onReady: PropTypes.func, // passes back `duration`
-  onPlay: PropTypes.func,
-  onPause: PropTypes.func,
-  onMute: PropTypes.func,
-  onUnmute: PropTypes.func,
-  onTimeUpdate: PropTypes.func, // passes back `currentTime`, `progress` and `duration`
-  onEnd: PropTypes.func,
-  onClick: PropTypes.func,
-  onKeyPress: PropTypes.func,
+	videoContainerID: PropTypes.string,
+	playsInline: PropTypes.bool, // play inline on iPhone. avoid triggering native video player
+	disableBackgroundCover: PropTypes.bool, // do not apply cover effect (e.g. disable it for specific screen resolution or aspect ratio)
+	style: PropTypes.object,
+	videoStyles: PropTypes.object,
+	className: PropTypes.string,
+	videoClasses: PropTypes.string,
+	containerWidth: PropTypes.number,
+	containerHeight: PropTypes.number,
+	src: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
+	poster: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+	horizontalAlign: PropTypes.number,
+	verticalAlign: PropTypes.number,
+	preload: PropTypes.string,
+	muted: PropTypes.bool, // required to be set to true for auto play on mobile in combination with 'autoPlay' option
+	volume: PropTypes.number,
+	loop: PropTypes.bool,
+	autoPlay: PropTypes.bool,
+	extraVideoElementProps: PropTypes.object,
+	startTime: PropTypes.number,
+	tabIndex: PropTypes.number,
+	onReady: PropTypes.func, // passes back `duration`
+	onPlay: PropTypes.func,
+	onPause: PropTypes.func,
+	onMute: PropTypes.func,
+	onUnmute: PropTypes.func,
+	onTimeUpdate: PropTypes.func, // passes back `currentTime`, `progress` and `duration`
+	onEnd: PropTypes.func,
+	onClick: PropTypes.func,
+	onKeyPress: PropTypes.func,
 };
 
 BackgroundVideo.defaultProps = {
-  playsInline: true,
-  disableBackgroundCover: false,
-  style: {},
-  horizontalAlign: 0.5,
-  verticalAlign: 0.5,
-  preload: 'auto',
-  muted: true,
-  volume: 1,
-  loop: true,
-  autoPlay: true,
-  extraVideoElementProps: {},
-  startTime: 0,
-  tabIndex: 0,
-  onReady: noop,
-  onPlay: noop,
-  onPause: noop,
-  onMute: noop,
-  onUnmute: noop,
-  onTimeUpdate: noop,
-  onEnd: noop,
-  onClick: noop,
-  onKeyPress: noop,
+	playsInline: true,
+	disableBackgroundCover: false,
+	style: {},
+	horizontalAlign: 0.5,
+	verticalAlign: 0.5,
+	preload: 'auto',
+	muted: true,
+	volume: 1,
+	loop: true,
+	autoPlay: true,
+	extraVideoElementProps: {},
+	startTime: 0,
+	tabIndex: 0,
+	onReady: noop,
+	onPlay: noop,
+	onPause: noop,
+	onMute: noop,
+	onUnmute: noop,
+	onTimeUpdate: noop,
+	onEnd: noop,
+	onClick: noop,
+	onKeyPress: noop,
 };
